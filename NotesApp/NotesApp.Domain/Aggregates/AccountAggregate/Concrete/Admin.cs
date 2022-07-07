4ee36c97931674ae686a58b84b5f045bc4ccdc78
halitdogmen <50083626+halitdogmen@users.noreply.github.com>
2022-07-08 01:35:14 +0300

    Domain Layer Admin user is created.

---
+++ NotesApp/NotesApp.Domain/Aggregates/AccountAggregate/Concrete/Admin.cs
@@ -0,0 +1,28 @@
+﻿using NotesApp.Domain.Aggregates.AccountAggregate.Abstracts;
+using NotesApp.Domain.Aggregates.AccountAggregate.ValueObjects;
+using SeedWork.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesApp.Domain.Aggregates.AccountAggregate.Concrete
+{
+    public class Admin:Account
+    {
+        public string NickName { get; private set; }
+
+        public Admin(string nickname,Email email, byte[] passwordHash, byte[] passwordSalt) : base(email, passwordHash, passwordSalt)
+        {
+            SetNickName(nickname);
+        }
+
+        private void SetNickName(string nickname)
+        {
+            if (string.IsNullOrEmpty(nickname)) throw new AttributeNotValidException($"{nameof(nickname)} can not be null or empty");
+
+            NickName = nickname;
+        }
+    }
+}
