089398ea48f94cd2ed7d16c24d974c8ee7e08c6f
halitdogmen <50083626+halitdogmen@users.noreply.github.com>
2022-07-08 02:38:34 +0300

    application Layers is configured for admin

---
+++ NotesApp/NotesApp.Application.Contracts/DTOs/Concrete/AdminDTO.cs
@@ -0,0 +1,14 @@
+﻿using NotesApp.Application.Contracts.DTOs.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesApp.Application.Contracts.DTOs.Concrete
+{
+    public class AdminDTO:AccountDTO
+    {
+        public string NickName { get; set; }
+    }
+}
