2688cf315ef6e4f8c0ef1e32db80180ffe37b8ca
halitdogmen <50083626+halitdogmen@users.noreply.github.com>
2022-07-07 23:05:51 +0300

    fixed abstraction.

--- NotesApp/NotesApp.Application.Contracts/DTOs/Concrete/StandartUserDTO.cs
+++ NotesApp/NotesApp.Application.Contracts/DTOs/Concrete/StandartUserDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NotesApp.Application.Contracts.DTOs.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace NotesApp.Application.Contracts.DTOs.Concrete
 {
-    public class StandartUserDTO
+    public class StandartUserDTO:AccountDTO
     {
         public string Name { get; private set; }
         public string Lastname { get; private set; }
