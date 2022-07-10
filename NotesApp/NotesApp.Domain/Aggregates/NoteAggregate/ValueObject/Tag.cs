eb9850ed71911f58c76d7a431f82dfe36a816858
halitdogmen <50083626+halitdogmen@users.noreply.github.com>
2022-07-10 07:35:56 +0300

    Tag Value object comments are added.

--- NotesApp/NotesApp.Domain/Aggregates/NoteAggregate/ValueObject/Tag.cs
+++ NotesApp/NotesApp.Domain/Aggregates/NoteAggregate/ValueObject/Tag.cs
@@ -7,18 +7,33 @@ using System.Threading.Tasks;
 
 namespace NotesApp.Domain.Aggregates.NoteAggregate.ValueObject
 {
+    /// <summary>
+    ///  Represents Notes App Note Tags. Value Object
+    /// </summary>
     public record Tag
     {
-        // For caching
+        /// <summary>
+        /// Represents Tag Id. Normally it is not required. Ef Core OwnsMany doent require but App uses Caching. 
+        /// </summary>
         public Guid Id { get; private set; }
+        /// <summary>
+        /// Represents Tag Name
+        /// </summary>
         public string Name { get; private set; }
-
+        /// <summary>
+        /// Consructor
+        /// </summary>
+        /// <param name="name">Represents Tag name.</param>
         public Tag(string name)
         {
             Id = Guid.NewGuid();
             SetName(name);
         }
-
+        /// <summary>
+        /// Setter method for Tag name
+        /// </summary>
+        /// <param name="name">Represents Tag name.</param>
+        /// <exception cref="AttributeNotValidException"></exception>
         public void SetName(string name)
         {
             if (string.IsNullOrEmpty(name)) throw new AttributeNotValidException($"{nameof(name)} can not be null or empty");
